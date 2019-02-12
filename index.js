f4f467d3b2c1208b5dc62c211de6ceb281a18c43
Kyle Brown <kylelibrown@gmail.com>
2019-02-12 22:18:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase("hello")
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
 }
