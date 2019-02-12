1156c098eb479c3818a7e41928645a158f9c2d54
Kyle Brown <kylelibrown@gmail.com>
2019-02-12 22:22:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can\'t hear you!"
   }
-}
-
-function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-}
+
